c9f9a58d7237a45e670f73447baa7a0bffda73de
Aidan Kwok <aidankwok@gmail.com>
2026-03-31 20:13:15 -0400

    Changed aidan_mac address

--- ros2_ws/build/drone_launch/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/aidankwok/drone/ros2_ws/install/autonomy_py/share/autonomy_py/package.sh"
--- ros2_ws/build/rgb_camera_py/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/aidankwok/drone/ros2_ws/install/rgb_camera_py'
